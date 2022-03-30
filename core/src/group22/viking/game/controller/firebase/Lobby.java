c85b5268e956f3af3d1132de9a2dd1546e3c7d37
Cornelius <cor.reb@posteo.de>
2022-03-30 18:46:23 +0200

    create and join lobby process listener

--- core/src/group22/viking/game/controller/firebase/Lobby.java
+++ core/src/group22/viking/game/controller/firebase/Lobby.java
@@ -1,14 +1,33 @@
 package group22.viking.game.controller.firebase;
 
+import java.util.Map;
+
 public class Lobby extends FirebaseDocument{
+
+    public enum State {
+        UNDEFINED("undefined"),
+        OPEN("open"),
+        GUEST_JOINED("guest_joined"),
+        GUEST_READY("guest_ready"),
+        GUEST_LEFT("guest_left"),
+        RUNNING("running");
+
+        public final String label;
+
+        private State(String label) {
+            this.label = label;
+        }
+    }
 
     public final static String KEY_HOST = "host";
     public final static String KEY_GUEST = "guest";
+    public final static String KEY_STATE = "open";
 
     public final static String GUEST_FIELD_DUMMY = "__open__";
 
     private String hostId;
     private String guestId;
+    private State state;
 
     /**
      * Dummy constructor.
@@ -17,19 +36,48 @@ public class Lobby extends FirebaseDocument{
         super("dummy");
         this.guestId = "dummy";
         this.hostId = "dummy";
+        this.state = State.UNDEFINED;
     }
 
+
+    /**
+     * Create new open lobby.
+     *
+     * @param id
+     * @param hostId
+     */
     public Lobby(String id, String hostId) {
         super(id);
         this.hostId = hostId;
+        this.state = State.OPEN;
         this.guestId = GUEST_FIELD_DUMMY;
     }
 
-    public void addGuestId(String guestId) {
+    public Lobby(String documentId) {
+        super(documentId);
+        this.hostId = null;
+        this.state = State.UNDEFINED;
+        this.guestId = GUEST_FIELD_DUMMY;
+    }
+
+    public void joinGuest(String guestId) {
         this.guestId = guestId;
+        this.state = State.GUEST_READY;
     }
 
     public boolean isFull() {
         return guestId != GUEST_FIELD_DUMMY;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public String getGuestId() {
+        return guestId;
     }
 }
--- core/src/group22/viking/game/controller/firebase/LobbyCollection.java
+++ core/src/group22/viking/game/controller/firebase/LobbyCollection.java
@@ -1,5 +1,6 @@
 package group22.viking.game.controller.firebase;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
@@ -11,7 +12,7 @@ public class LobbyCollection extends FirebaseCollection{
 
     private String currentLobbyId;
 
-    public final static int ID_LENGTH = 5;
+    public final static int ID_LENGTH = 4;
 
     public LobbyCollection(FirebaseInterface firebaseInterface) {
         super(firebaseInterface, new Lobby());
@@ -24,24 +25,172 @@ public class LobbyCollection extends FirebaseCollection{
         return (Lobby) get(currentLobbyId);
     }
 
-    public void openLobby(Profile profile, OnPostDataListener listener) {
-
-    }
-
-    public void joinLobby(final String id, Profile profile, final OnCollectionUpdatedListener listener) {
-
-        final LobbyCollection that = this;
-        firebaseInterface.get(name, id, new OnGetDataListener() {
+    public void createLobby(
+            final Profile profile,
+            final OnCollectionUpdatedListener gotIdListener,
+            final OnCollectionUpdatedListener guestJoinedListener)
+    {
+        final String lobbyId = generateId();
+        firebaseInterface.get(name, lobbyId, new OnGetDataListener() {
             @Override
             public void onGetData(String documentId, Map<String, Object> data) {
-                Lobby lobby = new Lobby(id, (String) data.get(Lobby.KEY_HOST));
-                that.currentLobbyId = "";
+                // try again by recursive call
+                createLobby(profile, gotIdListener, guestJoinedListener);
+            }
+
+            @Override
+            public void onFailure() {
+                // great! lobby id does not exist!
+                addNewLobby(lobbyId, profile, gotIdListener, guestJoinedListener);
+            }
+        });
+    }
+
+    private void addNewLobby(
+            String lobbyId,
+            final Profile profile,
+            final OnCollectionUpdatedListener gotIdListener,
+            final OnCollectionUpdatedListener guestJoinedListener)
+    {
+        Map<String, Object> lobbyValues = new HashMap<String, Object>() {{
+            put(Lobby.KEY_HOST, profile.getId());
+            put(Lobby.KEY_GUEST, Lobby.GUEST_FIELD_DUMMY);
+            put(Lobby.KEY_STATE, Lobby.State.OPEN);
+        }};
+
+        firebaseInterface.addOrUpdateDocument(name, lobbyId, lobbyValues, new OnPostDataListener() {
+            @Override
+            public void onSuccess(String documentId) {
+                Lobby lobby = new Lobby(documentId, profile.getId());
+                add(documentId, lobby);
+                currentLobbyId = documentId;
+
+                addWaitForJoinListener(lobby, guestJoinedListener);
+                gotIdListener.onSuccess(lobby);
+            }
+
+            @Override
+            public void onFailure() {
+                System.out.println("LobbyCollection: new lobby not added to server.");
+                gotIdListener.onFailure();
+            }
+        });
+    }
+
+    private void addWaitForJoinListener(Lobby lobby, final OnCollectionUpdatedListener listener) {
+        // TODO remove listener again, when game started
+        firebaseInterface.setOnValueChangedListener(name, lobby, new OnGetDataListener() {
+            @Override
+            public void onGetData(String documentId, Map<String, Object> data) {
+                Lobby lobby = (Lobby) get(documentId);
+
+                String guestId = (String) data.get(Lobby.KEY_GUEST);
+                if (guestId == Lobby.GUEST_FIELD_DUMMY) return;
+
+                String stateString = (String) data.get(Lobby.KEY_STATE);
+                if(Lobby.State.GUEST_LEFT.label.equals(stateString)) {
+                    System.out.println("LobbyCollection: user left again");
+                    listener.onFailure();
+                    return;
+                }
+
+                lobby.joinGuest(guestId);
+
                 listener.onSuccess(lobby);
             }
 
             @Override
             public void onFailure() {
+                System.out.println("LobbyCollection: listener on lobby not set.");
+                listener.onFailure();
+            }
+        });
+    }
+
+    public void tryToJoinLobbyById(
+            final String id,
+            final Profile profile,
+            final OnCollectionUpdatedListener gotLobbyListener,
+            final OnCollectionUpdatedListener startGameListener)
+    {
+        firebaseInterface.get(name, id, new OnGetDataListener() {
+            @Override
+            public void onGetData(String documentId, Map<String, Object> data) {
+                Lobby lobby = new Lobby(documentId);
+                for(Map.Entry<String, Object> e : data.entrySet()) {
+                    try {
+                        lobby.set(e.getKey(), e.getValue());
+                    } catch (FieldKeyUnknownException exception) {
+                        exception.printStackTrace();
+                    }
+                }
+                // check if lobby open
+                if(lobby.getState() != Lobby.State.OPEN) {
+                    System.out.println("LobbyCollection: Lobby is not open.");
+                    gotLobbyListener.onFailure();
+                    return;
+                }
+                add(lobby.getId(), lobby);
+                currentLobbyId = lobby.getId();
+                joinLobby(lobby, profile, gotLobbyListener, startGameListener);
+            }
+
+            @Override
+            public void onFailure() {
                 System.out.println("LobbyCollection: No lobby found.");
+                gotLobbyListener.onFailure();
+            }
+        });
+    }
+
+    private void joinLobby(
+            final Lobby lobby,
+            final Profile profile,
+            final OnCollectionUpdatedListener gotLobbyListener,
+            final OnCollectionUpdatedListener startGameListener)
+    {
+        Map<String, Object> lobbyValues = new HashMap<String, Object>(){{
+            put(Lobby.KEY_GUEST, profile.getId());
+            put(Lobby.KEY_STATE, Lobby.State.GUEST_READY);
+        }};
+
+        firebaseInterface.addOrUpdateDocument(name, lobby.getId(), lobbyValues, new OnPostDataListener() {
+            @Override
+            public void onSuccess(String documentId) {
+                lobby.joinGuest(profile.getId());
+                addWaitForStartListener(lobby, startGameListener);
+                gotLobbyListener.onSuccess(lobby);
+            }
+
+            @Override
+            public void onFailure() {
+                System.out.println("LobbyCollection: Joining lobby failed.");
+                gotLobbyListener.onFailure();
+            }
+        });
+    }
+
+    private void addWaitForStartListener(final Lobby lobby, final OnCollectionUpdatedListener listener) {
+        firebaseInterface.setOnValueChangedListener(name, lobby, new OnGetDataListener() {
+            @Override
+            public void onGetData(String documentId, Map<String, Object> data) {
+                String stateString = (String) data.get(Lobby.KEY_STATE);
+
+                if(!Lobby.State.RUNNING.equals(stateString)) return;
+
+                if(!lobby.getGuestId().equals((String) data.get(Lobby.KEY_GUEST))) {
+                    System.out.println("LobbyCollection: Some one else joined the lobby and started.");
+                    listener.onFailure();
+                    return;
+                }
+
+                lobby.setState(Lobby.State.RUNNING);
+                listener.onSuccess(lobby);
+            }
+
+            @Override
+            public void onFailure() {
+                System.out.println("LobbyCollection: Failed placing the listener.");
                 listener.onFailure();
             }
         });
