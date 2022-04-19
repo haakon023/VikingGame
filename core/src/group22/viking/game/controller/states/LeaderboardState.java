50ab23e1da22eb9ccb14673366030c288ca76c63
Sascha <43199203+smeyer98@users.noreply.github.com>
2022-04-19 19:11:05 +0200

    Update LeaderboardState.java

--- core/src/group22/viking/game/controller/states/LeaderboardState.java
+++ core/src/group22/viking/game/controller/states/LeaderboardState.java
@@ -4,21 +4,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
+import java.util.ArrayList;
+
 import group22.viking.game.controller.GameStateManager;
 import group22.viking.game.controller.VikingGame;
+import group22.viking.game.controller.firebase.Profile;
+import group22.viking.game.controller.firebase.ProfileCollection;
 import group22.viking.game.view.LeaderboardView;
 
 
 public class LeaderboardState extends State {
 
-
+    private final ProfileCollection profileCollection;
+    private ArrayList<Profile> leaderboard;
 
     public LeaderboardState(VikingGame game) {
         super(new LeaderboardView(game.getBatch(), game.getCamera()), game);
         Gdx.input.setInputProcessor(view.getStage());
         addListenersToButtons();
 
+        this.profileCollection = game.getProfileCollection();
+        leaderboard = profileCollection.getLeaderboard();
+
         System.out.println("LEADERBOARD STATE CREATED");
+        System.out.println(leaderboard);        //TODO
     }
 
     @Override
