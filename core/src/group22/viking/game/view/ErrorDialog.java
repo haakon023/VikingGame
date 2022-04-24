f27bf2ac7854ea3c25b6f96225880cd3400570c5
Sascha <43199203+smeyer98@users.noreply.github.com>
2022-04-24 14:24:57 +0200

    add popState to ErrorDialog

--- core/src/group22/viking/game/view/ErrorDialog.java
+++ core/src/group22/viking/game/view/ErrorDialog.java
@@ -3,21 +3,29 @@ package group22.viking.game.view;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
+import group22.viking.game.controller.GameStateManager;
 import group22.viking.game.models.Assets;
 
 public class ErrorDialog extends Dialog {
+
+    private boolean popState;
 
     public ErrorDialog(String title, String content, Skin skin) {
         super(title, skin);
         text("\n  " + content + "  \n");
         button("\n    O.K.    \n");
+        this.popState = false;
     }
 
     @Override
     protected void result(Object object) {
-
+        if(popState) {
+            GameStateManager.getInstance().pop();
+        }
     }
 
-
-
+    public ErrorDialog popState() {
+        this.popState = true;
+        return this;
+    }
 }
